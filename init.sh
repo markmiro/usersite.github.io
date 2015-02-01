84f0132f9127ff05f6f4198c5356e0100325bb80
Mark Miro <markmirony+github@gmail.com>
2015-01-31 16:34:56 -0800

    Seeing if we can just move all script off codeship and into the init script

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@
 git config --global user.name "Mark Miro"
 git config --global user.email "markmirony+github@gmail.com"
 git remote set-url origin git@github.com:markmiro/usersite.github.io.git
+
+npm install
 npm install -g bower
 bower install
