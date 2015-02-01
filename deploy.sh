09d0fd4ef34a5e17080e6c1f0af16ff8af74f276
Mark Miro <markmirony+github@gmail.com>
2015-01-31 16:21:16 -0800

    Added stuff to init

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-git config --global user.name "CI-Server"
-git config --global user.email "markmirony+github@gmail.com"
-npm install -g bower
-bower install
 gulp deploy
--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git config --global user.name "Mark Miro"
+git config --global user.email "markmirony+github@gmail.com"
 npm install -g bower
 bower install
