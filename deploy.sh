d5a7eae373790ba632062512b385f850a3c9f7c9
Mark Miro <markmirony+github@gmail.com>
2015-01-31 16:13:19 -0800

    Removed travis-related code. Just going to make it work on Codeship for now

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-git config --global user.name "Travis-CI"
+git config --global user.name "CI-Server"
 git config --global user.email "markmirony+github@gmail.com"
-git remote set-url origin git@github.com:markmiro/usersite.github.io.git
 npm install -g bower
 bower install
 gulp deploy
