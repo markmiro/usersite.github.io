531c21c7c74ae004bd5dfbbcd92f7ab1c6eaf245
Mark Miro <markmirony+github@gmail.com>
2015-01-31 16:26:56 -0800

    Added git repo url to init script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+git config --global user.name
+git config --global user.email
 gulp deploy
--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 git config --global user.name "Mark Miro"
 git config --global user.email "markmirony+github@gmail.com"
+git remote set-url origin git@github.com:markmiro/usersite.github.io.git
 npm install -g bower
 bower install
